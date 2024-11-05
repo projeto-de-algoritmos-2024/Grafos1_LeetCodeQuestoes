7178e9fd14fd7e74f480886c5e6522ab5c64a991
Cássio Reis <csreis04@gmail.com>
2024-11-04 22:28:34 -0300

    Criação da lista de adjacências

--- questao1/possibleBipartition.c
+++ questao1/possibleBipartition.c
@@ -1,3 +1,21 @@
+typedef struct No {
+    int x;
+    struct No *prox;
+} No;
+
 bool possibleBipartition(int n, int** dislikes, int dislikesSize, int* dislikesColSize) {
-    
+    No **adj = calloc(n + 1, sizeof(No *));
+
+    for (int i = 0; i < dislikesSize; i++)
+    {
+        No *novo = malloc(sizeof(No));
+        novo->x = dislikes[i][1];
+        novo->prox = NULL;
+
+        if (adj[dislikes[i][0]] != NULL)
+            novo->prox = adj[dislikes[i][0]];
+
+        adj[dislikes[i][0]] = novo;
+    }
+
 }
