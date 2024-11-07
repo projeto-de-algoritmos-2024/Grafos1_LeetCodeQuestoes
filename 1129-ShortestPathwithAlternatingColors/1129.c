882cc765e9714b6015365ed0039e6743069d5478
DeM4rcio <henriquemarcio170@gmail.com>
2024-11-07 20:05:24 -0300

    operação com lista de adj

--- 1129-ShortestPathwithAlternatingColors/1129.c
+++ 1129-ShortestPathwithAlternatingColors/1129.c
@@ -8,8 +8,12 @@ typedef struct celula{
 }celula;
 
 celula* criar_lista(int n){
-    celula *lista;
+    celula **lista;
     lista = (int*)malloc(sizeof(int)*n);
+    int valor = n;
+    for(int i = 0; i<=n;i++){
+        lista[i]->valor = i;
+    }
     return lista;
 }
 
@@ -22,13 +26,27 @@ void inserir_aresta(int v, int num, celula **lista, int n){
     for(int i = 0; i <= n;i++){
         if(lista[i] == num){
             while(lista[i]->prox != NULL){
+                nova_celula->prox= NULL;
+                nova_celula->valor = num;
                 lista[i]->prox = nova_celula;
-                nova_celula ->prox = NULL;
+                
             }
         }
     }
 }
 
 int* shortestAlternatingPaths(int n, int** redEdges, int redEdgesSize, int* redEdgesColSize, int** blueEdges, int blueEdgesSize, int* blueEdgesColSize, int* returnSize) {
+    celula *adjv = criar_lista(n);
+    celula *adja = criar_lista(n);
+    int status = 0; // 0 - red  1 - azul
+
+    for(int i = 0; i <= redEdgesSize; i++){
+        inserir_aresta(redEdges[i],redEdges[i], adjv,redEdgesSize );
+    }
+    for(int i = 0; i <= blueEdgesSize; i++){
+        inserir_aresta(blueEdges[i],blueEdges[i], adja,blueEdgesSize );
+    }
+
     
+
 }
