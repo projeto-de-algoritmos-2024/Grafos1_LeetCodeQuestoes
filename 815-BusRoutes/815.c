5624af6124e21ea7235e4d7ac56731ff59f679d4
Cássio Reis <csreis04@gmail.com>
2024-11-07 15:28:07 -0300

    Criação da lista de adjacências de  paradas com rotas

--- 815-BusRoutes/815.c
+++ 815-BusRoutes/815.c
@@ -7,4 +7,12 @@ typedef struct No {
 
 int numBusesToDestination(int** routes, int routesSize, int* routesColSize, int source, int target) {
     No **adj = calloc(MAX, sizeof(No *));
+
+    for (int i = 0; i < routesSize; i++)
+    {
+        for (int j = 0; j < routesColSize[i]; j++) 
+            insere_aresta(adj, routes[i][j], i);
+    }
+
+    return 0;
 }
